052f7a68f8120a6a7b1062f519b3ab55396a0752
Alejandro Vargas <eiviertest@gmail.com>
2019-10-11 13:02:27 -0500

    Actualizacion de modulo AllinOne

---
+++ AllinOne/app/src/main/java/mx/edu/utng/completeapp/MainActivity.kt
@@ -0,0 +1,37 @@
+package mx.edu.utng.completeapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.Menu
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT:Long=6000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Crear un objeto de Animation
+        val animacion: Animation = AnimationUtils.loadAnimation(this, R.anim.animacion )
+        txvUno.startAnimation(animacion)
+        hola.startAnimation(animacion)
+
+
+
+        //Se inicia la actividad principal
+        Handler().postDelayed({
+            startActivity(Intent(this,  PrincipalActivity::class.java))
+            finish()
+        },SPLASH_TIME_OUT)
+
+
+    }
+
+}
+
